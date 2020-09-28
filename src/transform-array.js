2f2f94626a7818201dc8648ff637ad24c8e61c21
Dima Bogush <bogusd66@gmail.com>
2020-09-28 17:17:11 +0300

    feat: my answer for task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const arrCopy = arr.concat();
+  const result = [];
+
+  for (let i = 0; i < arrCopy.length; i += 1) {
+    if (arrCopy[i] === '--discard-next') {
+      if (arrCopy[i + 1] !== '' && arrCopy[i + 1] !== undefined) {
+        arrCopy[i + 1] = '';
+      }
+      arrCopy[i] = '';
+    } else if (arrCopy[i] === '--discard-prev') {
+      if (arrCopy[i - 1] !== '' && arrCopy[i - 1] !== undefined) {
+        arrCopy[i - 1] = '';
+      }
+      arrCopy[i] = '';
+    } else if (arrCopy[i] === '--double-next') {
+      if (arrCopy[i + 1] !== '' && arrCopy[i + 1] !== undefined) {
+        arrCopy[i] = arrCopy[i + 1];
+      } else {
+        arrCopy[i] = '';
+      }
+    } else if (arrCopy[i] === '--double-prev') {
+      if (arrCopy[i - 1] !== '' && arrCopy[i - 1] !== undefined) {
+        arrCopy[i] = arrCopy[i - 1];
+      } else {
+        arrCopy[i] = '';
+      }
+    }
+  }
+
+  for (let i = 0; i < arrCopy.length; i += 1) {
+    if (arrCopy[i] !== '') {
+      result.push(arrCopy[i])
+    }
+  }
+  
+  return result
 };
